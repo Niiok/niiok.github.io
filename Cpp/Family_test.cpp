7725da3c403510aad778e7b021d09663a84487a7
Niiok <Niiok@users.noreply.github.com>
2022-02-24 17:27:49 +0000

    deploy: ae85965b540b745f9c0e3601a54505744f18701d

---
+++ Cpp/Family_test.cpp
@@ -0,0 +1,25 @@
+#include "Family.h"
+
+int prob();
+
+int main()
+{
+	int wait;
+
+	prob();
+
+	std::cin >> wait;
+
+	return 0;
+}
+
+int prob()
+{
+	int wait;
+	Me me_one;
+	Me me_two(2);
+	std::cin >> wait;
+
+
+	return 0;
+}
